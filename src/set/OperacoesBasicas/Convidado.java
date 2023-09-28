52cb3947f0bac6e04b37db420745441e2b5f41e1
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-09-28 17:05:06 -0300

    set interface

---
+++ src/set/OperacoesBasicas/Convidado.java
@@ -0,0 +1,28 @@
+package set.OperacoesBasicas;
+
+public class Convidado {
+    // atributos
+    private String nome;
+    private int condigoConvite;
+
+    public Convidado(String nome, int condigoConvite) {
+        this.nome = nome;
+        this.condigoConvite = condigoConvite;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getCondigoConvite() {
+        return condigoConvite;
+    }
+
+    @Override
+    public String toString() {
+        return "Convidado{" +
+                "nome='" + nome + '\'' +
+                ", condigoConvite=" + condigoConvite +
+                '}';
+    }
+}
